68e735d20541c27142e65ce7358676fbc724c4a7
Garrett Granroth <granrothge@ornl.gov>
2017-01-05 14:08:06 -0500

    adjust font size in figure text and add fit plot test

--- neutronpy/lsfit/fitter.py
+++ neutronpy/lsfit/fitter.py
@@ -493,6 +493,9 @@ class Fitter(object):
         # use the differnce in the number of decimal places from the parameter and the error
         # to determine how many places to keep  add one more decimal place for good measure.
         # if the difference is less than 1 make it 2.
+        print(parms)
+        print ("\n")
+        print(d_parms)
         n_dec=np.floor(np.log10(np.abs(parms)))-np.floor(np.log10(np.abs(d_parms)))+1
         n_dec[n_dec<1]=2
         n_dec=n_dec.astype('int')
@@ -581,9 +584,9 @@ class Fitter(object):
              If this is True it will give a plot of the residuals
 
         """
-        xin=self.data[0]
-        yin=self.data[1]
-        errin=self.data[2]
+        xin=self.data[1]
+        yin=self.data[2]
+        errin=self.data[3]
         plt.figure()
         if plot_residuals:
             ax1=plt.subplot2grid((2,3),(0,0),colspan=2)
@@ -599,7 +602,7 @@ class Fitter(object):
         # add text table
         parmtext=self.build_param_table(function_str)
         ax3=plt.subplot2grid((2,3),(0,2),rowspan=2)
-        ax3.text(0.1,0.5,parmtext)
+        ax3.text(0.1,0.5,parmtext,size=16)
         ax3.get_xaxis().set_visible(False)
         ax3.get_yaxis().set_visible(False)
         plt.show()
